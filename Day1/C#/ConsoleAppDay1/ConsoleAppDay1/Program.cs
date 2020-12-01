2be0b02b02bb2a641a13e803696ae3928dbc759a
Kristijan <kristijan@juniti.si>
2020-12-01 20:23:51 +0100

    Signed-off-by: Kristijan <kristijan@juniti.si>

--- Day1/C#/ConsoleAppDay1/ConsoleAppDay1/Program.cs
+++ Day1/C#/ConsoleAppDay1/ConsoleAppDay1/Program.cs
@@ -9,57 +9,68 @@ namespace ConsoleAppDay1
     {
         private static void Main()
         {
-            var text = File.ReadAllText(@"C:\Users\Kristijan\Desktop\AdventOfCode\Day1\input.txt");
 
-           var list = new List<string>(text.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries));
+            var text = File.ReadAllText(@"C:\Users\Kristijan\Desktop\AdventOfCode\Day1\C#\input.txt");
 
+            var list = new List<string>(text.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries));
 
-            var val1 = 0;
-            var val2 = 0;
+            var value = "";
 
-            while (val1 == 0 && val2 == 0)
+            while (value != "0")
             {
+
+                Console.WriteLine("+--------------------+");
+                Console.WriteLine("+ 1 for part I       +");
+                Console.WriteLine("+ 2 for part II      +");
+                Console.WriteLine("+ 0 for EXIT         +");
+                Console.WriteLine("+--------------------+");
+                value = Console.ReadLine();
+
+                switch (value)
+                {
+                    case "1":
+                        Console.WriteLine("rez = " + PartI(list));
+                        break;
+                    case "2":
+                        Console.WriteLine("rez = " + PartII(list));
+                        break;
+                }
+            }
+        }
+
+        private static string PartI(IReadOnlyCollection<string> list)
+        {
+
                 foreach (var l1 in list)
                 {
                     var value1 = int.Parse(l1);
                     foreach (var value2 in list.Select(int.Parse).Where(value2 => value1 + value2 == 2020))
                     {
-                        val1 = value1;
-                        val2 = value2;
+                        return (value1 * value2).ToString();
                     }
                 }
-            }
+                return "";
+        }
 
-            Console.WriteLine("rez = "+val1 * val2);
+        private static string PartII(IReadOnlyCollection<string> list)
+        {
 
-            val1 = 0;
-            val2 = 0;
-            var val3 = 0;
-
-            while (val1 == 0 && val2 == 0 && val3 == 0)
+            foreach (var l1 in list)
             {
-                foreach (var l1 in list)
+                var value1 = int.Parse(l1);
+
+                foreach (var l2 in list)
                 {
-                    var value1 = int.Parse(l1);
+                    var value2 = int.Parse(l2);
 
-                    foreach (var l2 in list)
+                    foreach (var value3 in list.Select(int.Parse).Where(value3 => value1 + value2 + value3 == 2020))
                     {
-                        var value2 = int.Parse(l2);
-
-                        foreach (var value3 in list.Select(int.Parse).Where(value3 => value1 + value2 +value3== 2020))
-                        {
-                            val1 = value1;
-                            val2 = value2;
-                            val3 = value3;
-                        }
-                        
+                        return (value1 * value2 * value3).ToString();
                     }
+
                 }
             }
-
-            Console.WriteLine("rez2 = "+val1 * val2 * val3);
-
-            Console.Read();
+            return "";
         }
     }
 }
